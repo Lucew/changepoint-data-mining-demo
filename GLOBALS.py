a1781a467a32d0397e147a94314262dec56c57da
Lucas <lucas.weber@fau.de>
2026-03-25 11:50:05 +0100

    adapt the globals to the maximum plotly shapes

--- GLOBALS.py
+++ GLOBALS.py
@@ -10,15 +10,21 @@ class Level(enum.Enum):
 DELETE_PASSWORD = os.environ.get("CPDASH_DELETE_PASSWORD", "changeme")
 
 # define some information
-DATA_FOLDER = os.environ.get("CPDASH_DATA_PATH", "./tmp-data-folder")
+DATA_FOLDER = os.environ.get("CPDASH_DATA_PATH", r"C:\Users\lucas\Data\CP_Anomaly\output_api")
 APPLICATION_LEVEL = Level.DEBUG
 PAGED = True
 DEFAULT_UPLOAD_TEXT = "📤 Please upload your ZIP file"
 DEFAULT_UPLOAD_TEXT_SUCCESS = f"✅ Uploaded"
 
 # define a reduced sensor set
-MAX_SIGNALS = 800
+MAX_SIGNALS = 500
+
+# define the maximum number of shape selections
+# !!!Please do not change this, if you are not absolutely certain that Plotly supports more shapes!!!
+# More Information: https://plotly.com/python/performance/
+MAX_PLOTLY_SHAPES = 8
 
 # check whether the data folder exists
 if not os.path.isdir(DATA_FOLDER):
-    raise ValueError(f"{DATA_FOLDER=} is not a valid directory.")
+    pass
+    # raise ValueError(f"{DATA_FOLDER=} is not a valid directory.")
