c04c3050a9ac1d84ff8a155c0858f38fd989a0ba
Lucas <lucas.weber@fau.de>
2026-03-30 10:51:31 +0200

    improve homepage

--- pages/home.py
+++ pages/home.py
@@ -1,11 +1,18 @@
 import dash
 from dash import html
 
+import util.styles as ustyle
+
+
 dash.register_page(__name__, path='/')
 
 
 def layout(session_id: str, folder_name: str, **kwargs):
     # check whether we have a folder
+    idea_text = "Navigate to one of the Pages in the Sidebar"
     if not folder_name:
-        return html.H1("Please upload a file using the sidebar.")
-    return html.Div(html.H1(["Navigate to one of the Pages in the Sidebar"]), style={"display": "flex", "justifyContent": "center", "alignItems": "center",  "minHeight": "100vh", "margin": "0"})
+        idea_text = "Please upload a file using the sidebar"
+
+    # make a style update
+    style_update = {"display": "flex", "justifyContent": "center", "alignItems": "center", "height": "100vh"}
+    return html.Div([html.Div([html.H1("Welcome to Changepoint!"), html.Br(), html.Div(idea_text, style={"display": "flex", "justifyContent": "center", "alignItems": "center", "fontSize": "Large"}), html.Br(), html.Br()], style=ustyle.div_styles['div'] | {"marginTop": "auto", "width": "30%"})] , style=style_update)
